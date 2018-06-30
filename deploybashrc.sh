2daf2db2741397d04c05f7c86a357aee2ea27ff0
Michael Hawkins <BadgerCode@users.noreply.github.com>
2018-06-30 18:56:40 +0100

    Removes uni and laptop entries from deploy

--- deploybashrc.sh
+++ deploybashrc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ -z "$1" ]]; then
-	echo "Pick an environment: laptop|desktop|uni|default"
+	echo "Pick an environment: desktop|work|default"
 	echo "E.g. deploybashrc.sh default"
 	exit
 fi
